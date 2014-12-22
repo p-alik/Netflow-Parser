51f53013728b38834d0ccc8ca6f2ada7f28673d6
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2014-12-22 18:11:47 +0100

    initial commit

---
+++ lib/Netflow/Parser.pm
@@ -0,0 +1,107 @@
+package Netflow::Parser;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Netflow::Parser - The great new Netflow::Parser!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Netflow::Parser;
+
+    my $foo = Netflow::Parser->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+IP Engineering, C<< <IP-Engineering at telecolumbus.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-netflow-parser at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Netflow-Parser>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Netflow::Parser
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Netflow-Parser>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Netflow-Parser>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Netflow-Parser>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Netflow-Parser/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 IP Engineering.
+
+This program is released under the following license: TC ownership
+
+
+=cut
+
+1; # End of Netflow::Parser
