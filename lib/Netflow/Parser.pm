27dd01bce11f9a6f6756e001f5a7ca09dda49143
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2014-12-23 14:51:44 +0100

    pass through boilerplate tests

--- lib/Netflow/Parser.pm
+++ lib/Netflow/Parser.pm
@@ -2,11 +2,11 @@ package Netflow::Parser;
 
 use 5.006;
 use strict;
-use warnings FATAL => 'all';
+use warnings;
 
 =head1 NAME
 
-Netflow::Parser - The great new Netflow::Parser!
+Netflow::Parser
 
 =head1 VERSION
 
@@ -14,93 +14,47 @@ Version 0.01
 
 =cut
 
-our $VERSION = '0.01';
+$Netflow::Parser::VERSION = '0.01';
 
 
 =head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
 
     use Netflow::Parser;
 
     my $foo = Netflow::Parser->new();
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 new
 
 =cut
 
-sub function1 {
+sub new {
 }
 
-=head2 function2
+=head2 parse
 
 =cut
 
-sub function2 {
+sub parse {
 }
 
 =head1 AUTHOR
 
-IP Engineering, C<< <IP-Engineering at telecolumbus.de> >>
+Alexei Pastuchov E<lt>palik at cpan.orgE<gt>.
 
-=head1 BUGS
+=head1 REPOSITORY
 
-Please report any bugs or feature requests to C<bug-netflow-parser at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Netflow-Parser>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Netflow::Parser
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Netflow-Parser>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Netflow-Parser>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Netflow-Parser>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Netflow-Parser/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
+L<git@github.com:p-alik/Netflow-Parser.git>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 IP Engineering.
+Copyright 2014 by Alexei Pastuchov E<lt>palik at cpan.orgE<gt>.
 
-This program is released under the following license: TC ownership
-
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
